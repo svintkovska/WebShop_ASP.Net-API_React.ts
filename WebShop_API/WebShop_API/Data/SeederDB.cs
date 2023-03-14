f9f907dd10570ac6057b71d9203803eb44d204c5
svintkovska <tanyasv97@gmail.com>
2023-03-14 23:19:23 +0200

    add bogus and seeder for products and product images

--- WebShop_API/WebShop_API/Data/SeederDB.cs
+++ WebShop_API/WebShop_API/Data/SeederDB.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using WebShop_API.Constants;
 using WebShop_API.Data.Entities.Identity;
 using WebShop_API.Data.Entities;
+using Bogus;
+using WebShop_API.Helpers;
+using System.Drawing.Imaging;
 
 namespace WebShop_API.Data
 {
@@ -30,6 +33,40 @@ namespace WebShop_API.Data
                     context.Categories.Add(cat);
                     context.SaveChanges();
                 }
+
+                if (!context.Products.Any())
+                {
+
+                    var testProduct = new Faker<ProductEntity>()
+                        .RuleFor(u => u.Name, (f, u) => f.Commerce.Product())
+                        .RuleFor(u => u.Price, (f, u) => decimal.Parse(f.Commerce.Price()))
+                        .RuleFor(u => u.DateCreated, (f, u) => DateTime.UtcNow)
+                        .RuleFor(u => u.Description, (f, u) => f.Commerce.ProductDescription())
+                        .RuleFor(u => u.CategoryId, (f, u) => 1);
+                    for (int i = 0; i < 20; i++)
+                    {
+                        var p = testProduct.Generate();
+                        context.Products.Add(p);
+                        context.SaveChanges();
+
+                        var testProductImage = new Faker<ProductImageEntity>()
+                       .RuleFor(u => u.DateCreated, (f, u) => DateTime.UtcNow)
+                       .RuleFor(u => u.ProductId, (f, u) => p.Id)
+                       .RuleFor(u => u.Name, f => f.Image.LoremFlickrUrl());
+
+                        for (int j = 0; j < 3; j++)
+                        {
+                            var img = testProductImage.Generate();
+                            string name = AddImage(app, img.Name);
+                            img.Name = name;
+                            context.ProductImages.Add(img);
+                            context.SaveChanges();
+                        }
+                    }
+
+                }
+
+
 
                 if (!context.Roles.Any())
                 {
@@ -64,6 +101,35 @@ namespace WebShop_API.Data
                     }
                 }
             }
+        }
+
+        private static string AddImage(IApplicationBuilder app, string urlImage)
+        {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var _configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+                string fileName = String.Empty;
+
+                if (urlImage != null)
+                {
+                    var bmp = ImageWorker.UrlToBitmap(urlImage);
+
+                    fileName = Path.GetRandomFileName() + ".jpg";
+                    string[] imageSizes = ((string)_configuration.GetValue<string>("ImageSizes")).Split(" ");
+                    foreach (var imageSize in imageSizes)
+                    {
+                        int size = int.Parse(imageSize);
+                        string dirSaveImage = Path.Combine(Directory.GetCurrentDirectory(), "images", $"{size}_{fileName}");
+
+                        var saveImage = ImageWorker.CompressImage(bmp, size, size, false, false);
+                        saveImage.Save(dirSaveImage, ImageFormat.Jpeg);
+                    }
+                    return fileName;
+
+                }
+            }
+            return null;
         }
     }
 
