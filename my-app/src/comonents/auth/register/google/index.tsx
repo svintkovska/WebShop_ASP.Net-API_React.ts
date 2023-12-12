6ac87d30b37caf819ad76d00a9673f758727742c
Tania <120820815+svintkovska@users.noreply.github.com>
2023-12-12 19:54:32 +0200

    final

--- my-app/src/comonents/auth/register/google/index.tsx
+++ my-app/src/comonents/auth/register/google/index.tsx
@@ -1,6 +1,8 @@
 import React, { ChangeEvent, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import http from '../../../../http';
+import imgCosmetics from "../../../../assets/images/cosmetics.jpg"
+import { useTranslation } from 'react-i18next';
 
 
 interface RegisterForm {
@@ -15,6 +17,7 @@ interface RegisterForm {
   
 
 const ContinueRegistration = () =>{
+  const { t } = useTranslation();
     const navigator = useNavigate();
 
     const location = useLocation();
@@ -73,140 +76,186 @@ const ContinueRegistration = () =>{
 
     return (
       <>
-        <div
-          className="d-flex flex-column justify-content-baseline align-items-center vh-100"
-          style={{
-            backgroundImage:
-              "url(https://img.freepik.com/premium-photo/black-friday-sale-banner-concept-design-shopping-bag-black-background-with-copy-space-3d-render_46250-3239.jpg)",
-            backgroundSize: "cover",
-          }}
-        >
-          <h1 className="mt-2 mb-3 text-center text-info">Finish Registration</h1>
+        <div className="cart-card">
+          <div className="row">
+            <div className="col-md-8 cart">
+              <div className="title">
+                <div className="row">
+                  <div className="col">
+                    <h1 className="mt-2 mb-3 text-center"  style={{color: "#e8baba"}}>
+                    {t('auth.finishRegistration')} 
+                    </h1>
 
-          <form
-            onSubmit={onSubmitHandler}
-            className="border border-info pb-2 pt-5 ps-5 pe-5"
-            style={{ width: 550, height: "auto" }}
-          >
-            <div className="d-flex flex-row justify-content-baseline align-items-center">
-              <div className="mb-3 me-5 text-info">
-                <label htmlFor="firstName" className="form-label">
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="firstName"
-                  value={state.firstName}
-                  onChange={onChangeInputHandler}
-                  placeholder="Enter First Name"
-                  required
-                />
-                <div className="invalid-feedback">
-                  Please enter a valid name.
-                </div>
-              </div>
+                    <form
+                      onSubmit={onSubmitHandler}
+                      className="my_form"
+                      style={{ width: 550, height: "auto" }}
+                    >
+                      <div className="d-flex flex-row justify-content-baseline align-items-center">
+                        <div className="mb-3 me-5">
+                         <label
+                            htmlFor="firstName"
+                            className="form-label"
+                            style={{color: "#e8baba"}}
+                          >
+                            {t('auth.firstName')} 
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            name="firstName"
+                            value={state.firstName}
+                            onChange={onChangeInputHandler}
+                            placeholder={t('auth.enterFirstName')} 
+                            required
+                          />
+                        </div>
 
-              <div className="mb-3 text-info">
-                <label htmlFor="lastName" className="form-label">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="lastName"
-                  value={state.lastName}
-                  onChange={onChangeInputHandler}
-                  placeholder="Enter Last Name"
-                  required
-                />
-                <div className="invalid-feedback">
-                  Please enter a valid name.
+                        <div className="mb-3">
+                          <label
+                            htmlFor="lastName"
+                            className="form-label"
+                            style={{color: "#e8baba"}}
+                          >
+                            {t('auth.lastName')} 
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            name="lastName"
+                            value={state.lastName}
+                            onChange={onChangeInputHandler}
+                            placeholder={t('auth.enterLastName')} 
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className="d-flex flex-row justify-content-baseline align-items-center">
+                        <div className="mb-3 me-5">
+                          <label
+                            htmlFor="username"
+                            className="form-label "
+                            style={{color: "#e8baba"}}
+                          >
+                            {t('auth.userName')} 
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            name="username"
+                            value={state.username}
+                            onChange={onChangeInputHandler}
+                            placeholder={t('auth.enterUserName')} 
+                            required
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <label
+                            htmlFor="email"
+                            className="form-label"
+                            style={{color: "#e8baba"}}
+                          >
+                            {t('auth.email')} 
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            name="email"
+                            value={state.email}
+                            onChange={onChangeInputHandler}
+                            placeholder={t('auth.enterEmail')} 
+                            required
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
+ 
+                      <div className="mt-1">
+                        <label htmlFor="uploadImage" className="form-label">
+                          <img
+                            src={
+                                            state.uploadImage == null
+                                              ? "http://localhost:5285/images/" +
+                                               "300_" +
+                                                 state.imagePath
+                                              : URL.createObjectURL(state.uploadImage)
+                                          }
+                            alt="select img"
+                            
+                            style={{ cursor: "pointer", width:"150px" }}
+                          />
+                        </label>
+                        <input
+                          type="file"
+                          className="d-none"
+                          name="uploadImage"
+                          id="uploadImage"
+                          onChange={onFileChangeHandler}
+                        />
+                      </div>
+
+                      <div className="text-center">
+                        <button type="submit" className="cart-btn mb-3" style={{width: "300px"}}>
+                        {t('auth.register')} 
+                        </button>
+                      </div>
+                      
+                    </form>
+
+                    <p className="text-center mt-3" style={{color: "#e8baba"}}>
+                    {t('auth.haveAccount')}?
+                      <Link to="/account/login">
+                        <button
+                          className="back-btn" style={{padding: "10px", width: "200px", marginLeft: 10 }}
+                        >
+                          {t('auth.login')}
+                        </button>
+                      </Link>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-
-            <div className="d-flex flex-row justify-content-baseline align-items-center">
-              <div className="mb-3 me-5 text-info">
-                <label htmlFor="username" className="form-label">
-                  User Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  value={state.username}
-                  onChange={onChangeInputHandler}
-                  placeholder="Enter User Name"
-                  required
-                />
-                <div className="invalid-feedback">
-                  Please enter a valid name.
-                </div>
-              </div>
-
-              <div className="mb-3 text-info">
-                <label htmlFor="email" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={state.email}
-                  placeholder="Enter Email"
-                  disabled={true}
-                />
-                <div className="invalid-feedback">
-                  Please enter a valid name.
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-1">
-              <label htmlFor="uploadImage" className="form-label">
-                <img
-                  src={
-                    state.uploadImage == null
-                      ? "http://localhost:5285/images/" +
-                        "300_" +
-                        state.imagePath
-                      : URL.createObjectURL(state.uploadImage)
-                  }
-                  alt="select img"
-                  width="150px"
-                  style={{ cursor: "pointer" }}
-                />
-              </label>
-              <input
-                type="file"
-                className="d-none"
-                name="uploadImage"
-                id="uploadImage"
-                onChange={onFileChangeHandler}
-              />
-            </div>
-
-            <div className="text-center">
-              <button type="submit" className="btn btn-info mb-3">
-                Finish
-              </button>
-            </div>
-            <hr></hr>
-          </form>
-          <p className="text-center text-info mt-3">
-            Have an account?
-            <Link to="/account/login">
-              <button
-                className="btn btn-outline-info"
-                style={{ marginLeft: 10 }}
-              >
-                Log In
-              </button>
-            </Link>
-          </p>
+            <div
+              className="col-md-4 summary"
+              style={{
+                backgroundImage: `url(${imgCosmetics})`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </div>
         </div>
       </>
+   
+
+    
+
+      //       <div className="text-center">
+      //         <button type="submit" className="btn btn-info mb-3">
+      //           Finish
+      //         </button>
+      //       </div>
+      //       <hr></hr>
+      //     </form>
+      //     <p className="text-center text-info mt-3">
+      //       Have an account?
+      //       <Link to="/account/login">
+      //         <button
+      //           className="btn btn-outline-info"
+      //           style={{ marginLeft: 10 }}
+      //         >
+      //           Log In
+      //         </button>
+      //       </Link>
+      //     </p>
+      //   </div>
+      //   </div>
+      //   </div>
+      //   </div>
+      //   </div>
+      //   </div>
+      // </>
     );
 }
 
